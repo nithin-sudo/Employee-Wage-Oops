ed8359dcbf03a7e9948a7630ba6dff979567d9f8
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-06 18:31:01 +0530

    Welcome Message

---
+++ EmployeeWage.java
@@ -0,0 +1,12 @@
+package employeewage;
+
+public class EmployeeWage {
+	public static void employeeWage()
+	{
+		System.out.println("Welcome to Employee Wage Problem using oops");
+	}
+	public static void main(String[] args){
+		EmployeeWage employeewage=new EmployeeWage();
+		employeewage.employeeWage();
+	}
+}
