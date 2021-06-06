d5906e7c5703620286e46c2dd3cefdc4e078285e
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-06 18:45:21 +0530

    added:Employee daily Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,15 +5,20 @@ public class EmployeeWage {
 	public static void employeeAttendance()
 	{
 		System.out.println("Welcome to Employee Wage Problem using oops");
+		int wage_per_hr=20;
+		int full_day_work_hr=8;
+		int salary;
 		double random=Math.floor(Math.random()*10)%2;
 		System.out.println("random value is:"+random);
 		if (random==IS_FULL_PRESENT) 
 		{
-			System.out.println("Employee is present");
+			System.out.println("employee is present");
+			salary=(wage_per_hr*full_day_work_hr);
+			System.out.println(salary);
 		}
 		else
 		{
-			System.out.println("Employee is absent");			
+			System.out.println("when employee is absent for the day his salary is zero");				
 		}
 	}
 	public static void main(String[] args){
