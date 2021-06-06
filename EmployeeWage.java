bbb660007e22be51dc8b9d8ecb22d6c2184c769e
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-06 19:48:19 +0530

    Added:EmployeeWage for given condition

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,7 @@
 package employeewage;
 
+import java.util.Random;
+
 public class EmployeeWage {
 	public static final int IS_FULL_PRESENT = 1;
 	public static final int IS_PART__PRESENT = 2;
@@ -7,26 +9,36 @@ public class EmployeeWage {
 	public static void employeeSalaryPerMonth() {
 		System.out.println("Welcome to Employee Wage Problem using oops");
 		int wage_per_hr=20;
-		int full_day_work_hr=8; 
-		int part_day_work_hr=4; 
+		int fullDayWorkHr=8; 
+		int partDayWorkHr=4; 
 		int salary;
 		int workingDays=20;
 		int salaryPerMonth=0;
-		for (int i=1;i<workingDays;i++)
-		{
-			double random= Math.floor(Math.random()*10)%2;
-			if (IS_FULL_PRESENT==random)
-			{
-				salary=(wage_per_hr*full_day_work_hr);
-				salaryPerMonth=(salaryPerMonth+salary);
+		int totalWorkingHrs=0;
+		int i;
+		  for (i=0;i<workingDays;i++)
+		  {
+				Random var = new Random();
+				int random;
+				random=var.nextInt(3);
+				switch(random) {
+				case 1:
+					salary=(wage_per_hr*fullDayWorkHr);
+					salaryPerMonth=(salaryPerMonth+salary);
+					totalWorkingHrs=(totalWorkingHrs+fullDayWorkHr);
+					break;
+				case 2:
+					salary=(wage_per_hr*partDayWorkHr);
+					salaryPerMonth=(salaryPerMonth+salary);
+					totalWorkingHrs=(totalWorkingHrs+partDayWorkHr);
+					break;
+			  }
+				if (totalWorkingHrs==100 || i==20)
+				{
+					break;
+				}
 			}
-			else if (IS_PART__PRESENT==random)
-			{
-				salary=(wage_per_hr*part_day_work_hr);
-				salaryPerMonth=(salaryPerMonth+salary);
-			}
-		}
-		System.out.println("salary per month is"+salaryPerMonth);				
+		System.out.println("salary is credited for" +totalWorkingHrs+ "hours"  +i+ "days"+salaryPerMonth);			
 }
 
 	public static void main(String[] args) {
